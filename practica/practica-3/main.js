fb062ea850fc832c6560e729a06e63a7ca6c88fc
LuisMata <mata.ramirez.1iv14@gmail.com>
2022-01-14 19:33:57 -0600

    responcive-400px

--- practica/practica-3/main.js
+++ practica/practica-3/main.js
@@ -1,3 +1,3 @@
-document.querySelector('.menu-btn').addEventListener('click', () => {
-    document.querySelector('.menu-btn').classList.toggle('show');
-});
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+  });ß
